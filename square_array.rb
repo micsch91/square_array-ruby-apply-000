ca40f08f69b967dacb185a0a7a2462f4f30566cd
Michael Schneider <mic.sch91@gmail.com>
2020-03-16 16:13:41 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,14 @@
 def square_array(array)
-  # your code here
+  array = [1,2,3]
+
+
+end
+
+def square_array(new_numbers)
+  new_numbers = [9,10,16,25]
+  puts "new_numbers[0] ** 2"
+  puts "new_numbers[1] ** 2"
+  puts "new_numbers[2] ** 2"
+  puts "new_numbers[3] ** 2"
+
 end
