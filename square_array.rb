3689f72adc6676ebb9f160c0339acc136379136d
Michael Schneider <mic.sch91@gmail.com>
2020-03-16 16:25:55 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,14 +1,9 @@
 def square_array(array)
-  array = [1,2,3]
+  square = []
 
+array.each {|num|
+  square.push(num*num)
 
-end
-
-def square_array(new_numbers)
-  new_numbers = [9,10,16,25]
-  puts "new_numbers[0] ** 2"
-  puts "new_numbers[1] ** 2"
-  puts "new_numbers[2] ** 2"
-  puts "new_numbers[3] ** 2"
+}
 
 end
